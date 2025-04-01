7fba12489d91a007e7f0d17292b5469671f79717
Guy Moalem <guy.moa@gmail.com>
2025-04-01 20:11:03 +0300

    changed dpi and added buttons (not functional yet)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,29 @@ import { drawBoard, chooseWords } from "./game.js";
 
 // Get the canvas and change its size
 const canvas = document.getElementById("game-canvas");
-canvas.width = 800;
-canvas.height = 600;
-
 const ctx = canvas.getContext("2d");
+
+const setCanvasDPI = (
+  canvas,
+  ctx,
+  width,
+  height,
+  dpi = window.devicePixelRatio
+) => {
+  const scaledWidth = width * dpi;
+  const scaledHeight = height * dpi;
+
+  canvas.width = scaledWidth;
+  canvas.height = scaledHeight;
+
+  canvas.style.width = `${width}px`;
+  canvas.style.height = `${height}px`;
+};
+
+const canvasWidth = 800,
+  canvasHeight = 600;
+
+setCanvasDPI(canvas, ctx, canvasWidth, canvasHeight);
 
 const rows = 4,
   cols = 5;
@@ -41,9 +60,11 @@ const tiles = Array.from({ length: rows }, (_, row) =>
 
 canvas.addEventListener("click", (event) => {
   const rect = canvas.getBoundingClientRect();
-  const mouseX = event.clientX - rect.left;
-  const mouseY = event.clientY - rect.top;
+  const dpi = window.devicePixelRatio;
 
+  const mouseX = (event.clientX - rect.left) * dpi;
+  const mouseY = (event.clientY - rect.top) * dpi;
+ 
   const clickedCol = Math.floor(mouseX / tileWidth);
   const clickedRow = Math.floor(mouseY / tileHeight);
 
