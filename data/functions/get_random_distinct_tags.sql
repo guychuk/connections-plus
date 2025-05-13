a107dfd22f7bf61d3edbf7e91467dbf6c9f862b2
Guy Moalem <guy.moa@gmail.com>
2025-05-13 16:24:02 +0300

    updated the scheme (tags table) and the get_random_distict_tags function

--- data/functions/get_random_distinct_tags.sql
+++ data/functions/get_random_distinct_tags.sql
@@ -1,10 +1,11 @@
-CREATE OR REPLACE FUNCTION get_random_distinct_tags(n integer)
+CREATE OR REPLACE FUNCTION get_random_distinct_tags(n integer, lang text)
 RETURNS SETOF tags AS $$
 BEGIN
   RETURN QUERY
   WITH distinct_tags AS (
     SELECT DISTINCT ON (tag) *
     FROM tags
+    WHERE language = lang
   )
   SELECT *
   FROM distinct_tags
